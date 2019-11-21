549f8f9e4155a12e02d83abdcbf3b7c0f9b5d612
Jacob Wailes <jkipod@live.com>
2019-11-20 21:06:31 -0800

    Remove Token Key

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const bot = new Client();
 ["aliases", "commands"].forEach(x => bot[x] = new Collection());
 ["console", "command", "event"].forEach(x => require(`./handlers/${x}`)(bot));
 
-bot.login(token);
+bot.login(process.env.BOT_TOKEN);
